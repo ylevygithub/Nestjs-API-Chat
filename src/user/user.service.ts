5f56890ae2012a3794d97715b257140b7d1a461c
yaacov <yaacov.levy@epitech.eu>
2024-09-11 20:48:28 +0200

    [ADD]: main, appModule and user Module/Controller/Service

---
+++ src/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  getUsers() {
+    return [
+      {
+        id: 1,
+        firstname: 'Yacs',
+      },
+    ];
+  }
+}
